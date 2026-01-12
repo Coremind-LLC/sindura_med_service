f1db0def3f16b3e8923b89d9e909a3e595e9c580
Khongor Batsaikhan <dwyanekhongor@gmail.com>
2026-01-12 14:39:13 +0800

    Updated

--- apps/examination/serializers.py
+++ apps/examination/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from apps.examination.models import Examination
 
 class ExaminationSerializer(serializers.ModelSerializer):
+    doctor_first_name = serializers.CharField(source="doctor.first_name", read_only=True)
+    doctor_last_name = serializers.CharField(source="doctor.last_name", read_only=True)
+    doctor_position = serializers.CharField(source="doctor.position", read_only=True)
+    examination_type_name = serializers.CharField(
+        source="examination_type.name", read_only=True
+    )
+
     class Meta:
         model = Examination
         fields = "__all__"
