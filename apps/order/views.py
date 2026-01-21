b3a966d859f220ca3045d932df7585dffd0e2e32
Khongor Batsaikhan <dwyanekhongor@gmail.com>
2026-01-21 16:50:37 +0800

    Updated

--- apps/order/views.py
+++ apps/order/views.py
@@ -1,18 +1,47 @@
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
 
 from apps.common.views import BaseViewSet
 from apps.order.models import Order
 from apps.order.serializers import OrderSerializer
+from apps.order.services import OrderService
 
 class OrderViewSet(BaseViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
     def get_permissions(self):
-        if self.request.method == "POST":
+        if self.action in ["create", "list"]:
             return [AllowAny()]
         return [IsAuthenticated()]
 
-    # @action(detail=False, methods=["get"], url_path="(?P<order_id>\d+)/approve")
-    # def approve(self, request, order_id):
+    # def create(self, request, *args, **kwargs):
+    #     serializer = self.get_serializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     order = serializer.save()
+    #     return Response(self.get_serializer(order).data)
+
+    # @action(detail=True, methods=["patch"], url_path="approve")
+    # def approve(self, request, pk=None):
+    #     order = self.get_object()
+    #     order, error = OrderService.approve(order.id)
+    #     if error:
+    #         return Response({"message": error}, status=400)
+    #     return Response({"message": "Order approved"})
+    #
+    # @action(detail=True, methods=["patch"], url_path="cancel")
+    # def cancel(self, request, pk=None):
+    #     order = self.get_object()
+    #     order, error = OrderService.cancel(order.id)
+    #     if error:
+    #         return Response({"message": error}, status=400)
+    #     return Response({"message": "Order cancelled"})
+    #
+    # @action(detail=False, methods=["post"], url_path="manual")
+    # def manual(self, request):
+    #     serializer = self.get_serializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     order = serializer.save()
+    #     return Response(self.get_serializer(order).data)
+
