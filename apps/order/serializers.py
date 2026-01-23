f7ae576a06c52830e2dd280887b4e67b0a4e3e03
Khongor Batsaikhan <dwyanekhongor@gmail.com>
2026-01-23 15:39:55 +0800

    Updated

--- apps/order/serializers.py
+++ apps/order/serializers.py
@@ -16,6 +16,10 @@ class OrderSerializer(serializers.ModelSerializer):
     examination_deposit_amount = serializers.DecimalField(
         source="examination.deposit_amount", read_only=True, max_digits=20, decimal_places=2
     )
+    examination_type_name = serializers.CharField(source="examination.examination_type.name", read_only=True)
+    examination_type_icon = serializers.CharField(source="examination.examination_type.icon", read_only=True)
+    examination_date = serializers.DateField(source="examination.date", read_only=True)
+    examination_time = serializers.TimeField(source="examination.time", read_only=True)
     invoice = InvoiceSerializer(read_only=True)
 
     class Meta:
