165a9f350b8ef354e074bf6468551b141b6c74e4
Khongor Batsaikhan <dwyanekhongor@gmail.com>
2026-01-30 15:22:18 +0800

    Updated

--- apps/examination_pack/views.py
+++ apps/examination_pack/views.py
@@ -33,6 +33,11 @@ class ExaminationPackViewSet(BaseViewSet):
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
