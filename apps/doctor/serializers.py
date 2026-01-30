4986602c833e11f09633ad2e4f25760a9b9be3e1
Khongor Batsaikhan <dwyanekhongor@gmail.com>
2026-01-30 11:26:27 +0800

    Updated

--- apps/doctor/serializers.py
+++ apps/doctor/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
 from apps.doctor.models import Doctor
+from apps.doctor.services import DoctorService
 
 class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
         fields = "__all__"
+
+    def validate_email(self, value):
+        instance_id = self.instance.id if self.instance else None
+
+        if DoctorService.exists_by_email(
+            email=value,
+            exclude_id=instance_id
+        ):
+            raise serializers.ValidationError("Email already exists")
+
+        return value
